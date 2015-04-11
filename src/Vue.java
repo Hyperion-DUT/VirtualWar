158b6f7b1062751931e165a161be74179c250e4d
Noxilex <dhoop59@hotmail.fr>
2015-04-11 21:40:56 +0200

    Adding Vue.java

--- src/Vue.java
+++ src/Vue.java
@@ -1,4 +1,16 @@
+package main;
 
 public class Vue {
+	private boolean estVisible;
+	
+	public Vue(){
+	}
 
+	public boolean estVisible() {
+		return estVisible;
+	}
+
+	public void setVisible(boolean estVisible) {
+		this.estVisible = estVisible;
+	}
 }
