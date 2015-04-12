aaa012e018d7af39ff468acb6d931c43ee2d0945
CDevCom <support@cdevcom.com>
2015-04-12 12:07:49 +0200

    Ajout des classes Action,Attaque et Deplacement (Base)

---
+++ src/Attaque.java
@@ -0,0 +1,15 @@
+
+public class Attaque extends Action {
+
+	public Attaque(Robot robot, Coordonnees direction) {
+		super(robot, direction);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void agit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/Deplacement.java
@@ -0,0 +1,15 @@
+
+public class Deplacement extends Action{
+
+	public Deplacement(Robot robot, Coordonnees direction) {
+		super(robot, direction);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void agit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
