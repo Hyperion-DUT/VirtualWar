b3e695f485d7830a77f9daa77f7d1c3b69faef32
BenjaminDeregnaucourt <benji94111995@gmail.com>
2015-04-14 21:27:45 +0200

    update Attaque.java

--- src/Attaque.java
+++ src/Attaque.java
@@ -4,16 +4,77 @@
  *
  */
 public class Attaque extends Action {
-
+	/**
+	 * encapsulation de la classe Robot avoir une cible
+	 */
+	Robot robotCible;
+	int porteeAttaque;
+	/**
+	 * Ce constructeur initialise une attaque à partir d'un robot et de coordonnees places en parametre.
+	 * @param robot
+	 * @param direction
+	 */
 	public Attaque(Robot robot, Coordonnees direction) {
 		super(robot, direction);
-		// TODO Auto-generated constructor stub
+
 	}
 
 	@Override
+	/**
+	 * Cette methode modifie la portee d'attaque en fonction de l'unite et modifie l'energie de l'unité et de sa cible apres l'attaque
+	 */
 	void agit() {
-		// TODO Auto-generated method stub
 		
+		if (this.getRobot() instanceof Tireur){
+			porteeAttaque=3;
+			if(attaquerTir(robotCible)==true){
+				this.getRobot().setEnergie(this.getRobot().getEnergie()-this.getRobot().getCoutAction());
+				robotCible.setEnergie(robotCible.getEnergie()-this.getRobot().getDegatTir());
+			}
+			
+		if (this.getRobot() instanceof Char){
+			porteeAttaque=10;
+			if(attaquerTir(robotCible)==true){
+				this.getRobot().setEnergie(this.getRobot().getEnergie()-this.getRobot().getCoutAction());
+				robotCible.setEnergie(robotCible.getEnergie()-this.getRobot().getDegatTir());
+			}
+		}	
+		if (this.getRobot() instanceof Piegeur &&(robotCible instanceof Tireur || robotCible instanceof Piegeur || robotCible instanceof Char)){
+			porteeAttaque=1;
+			if(attaquerTir(robotCible)==true){
+				this.getRobot().setEnergie(this.getRobot().getEnergie()-this.getRobot().getCoutAction());
+				robotCible.setEnergie(robotCible.getEnergie()-this.getRobot().getDegatMine());
+				}
+			}
+		
+		}
 	}
-
+	/**
+	 * Cette methode retourne un booleen qui exprime si le robot est capable d'attaquer sa cible
+	 * @param robotCible
+	 * @return
+	 */
+		public boolean attaquerTir(Robot robotCible){
+			if ((robotCible.getCoordonnees().getX()-this.getRobot().getCoordonnees().getX()<=porteeAttaque && robotCible.getCoordonnees().getX()-this.getRobot().getCoordonnees().getX()>-porteeAttaque) || (robotCible.getCoordonnees().getY()==this.getRobot().getCoordonnees().getY())){
+				return true;
+			}
+			if((robotCible.getCoordonnees().getY()-this.getRobot().getCoordonnees().getY()<porteeAttaque && robotCible.getCoordonnees().getY()-this.getRobot().getCoordonnees().getY()>porteeAttaque)&& (this.getRobot().getCoordonnees().getX()==robotCible.getCoordonnees().getX())){
+				return true;
+			}
+			return false;
+		}
+		/**
+		 * Cette methode retourne true si il est possible de poser une mine
+		 * @param cellule
+		 * @return
+		 */
+		public boolean PoserMine(Cellule cellule){
+			if (cellule.estVide()==true){
+				return true;
+			}
+			return false;
+		}
+	
 }
+	
+	
